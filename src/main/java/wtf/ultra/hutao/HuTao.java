9c9fabc8172b5552b42e9ce86fcc3f26667ca4f7
Ryan <bridgeplayer411@gmail.com>
2023-08-13 00:12:46 -0400

    reworked for higher frequency event fires

--- src/main/java/wtf/ultra/hutao/HuTao.java
+++ src/main/java/wtf/ultra/hutao/HuTao.java
@@ -7,24 +7,31 @@ import net.weavemc.loader.api.ModInitializer;
 import net.weavemc.loader.api.event.EventBus;
 import net.weavemc.loader.api.event.RenderGameOverlayEvent;
 
-
 import java.util.stream.IntStream;
 
+@SuppressWarnings("unused")
 public class HuTao implements ModInitializer {
     private static final ResourceLocation[] images;
+    private final long mspf = 100;
+    private long instant = 0;
     private int frame = 0;
 
     @Override
     public void preInit() {
         EventBus.subscribe(RenderGameOverlayEvent.Pre.class, event -> {
-            Minecraft.getMinecraft().getTextureManager().bindTexture(images[frame / 2]);
-            ScaledResolution resolution = new ScaledResolution(Minecraft.getMinecraft());
+            long now;
+            if ((now = System.currentTimeMillis()) - instant >= mspf) {
+                instant = now;
+                frame = (frame + 1) % images.length;
+            }
+
+            Minecraft mc = Minecraft.getMinecraft();
+            mc.getTextureManager().bindTexture(images[frame]);
+            ScaledResolution resolution = new ScaledResolution(mc);
             int w = 256, h = 256, u = 0, v = 0;
             int x = resolution.getScaledWidth() - w;
             int y = resolution.getScaledHeight() - h;
-            Minecraft.getMinecraft().ingameGUI.drawTexturedModalRect(x, y, u, v, w, h);
-
-            frame = (frame + 1) % 54;
+            mc.ingameGUI.drawTexturedModalRect(x, y, u, v, w, h);
         });
     }
 
